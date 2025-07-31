dcbc05a8d4a773ea4316d0521542fbb823bde73f
d <dumasdd9@gmail.com>
2025-07-31 00:32:36 -0500

    Changed the complete script to py audio

--- completescript.py
+++ completescript.py
@@ -1,56 +1,44 @@
-import sounddevice as sd
+import pyaudio
+import wave
 from vosk import Model, KaldiRecognizer, SetLogLevel
 import os
-import whisper
-from groq import Groq
-import subprocess
-import pyttsx3
 
-#Setting things up
+#Vosk Log Level
+SetLogLevel(0) # 0 = debug, 1 = info, 2 = warning, 3 = error, 4 = none
 
-#pyttsx3
-engine = pyttsx3.init()
-
-#Vosk model stuff and params
-model_path = "trncript/vosk-model-en-us-0.22"
+#Load vosk model
+model_path = "vosk-model-en-us-0.22"
 model = Model(model_path)
 recognizer = KaldiRecognizer(model, 16000)
-SetLogLevel(3) #0 = debug, 1 = info, 2 = warning, 3 = error, 4 = none
 
-#Recording params
-RATE = 16000
-CHUNK_SIZE = 1024
-CHANNELS = 2
+#Params
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 18000
+CHUNK_SIZE = 2048
 
-# def main():
-#     print("Recording started...")
-#     with sd.InputStream(channels=CHANNELS, samplerate=RATE, blocksize=CHUNK_SIZE) as stream:
-#         while True:
-#             data, overflow = stram.read(CHUNK_SIZE)
-#             if overflow:
-#                 print("Overflow Warning")
-#
-#             if recognizer.AcceptWaveform(data.tobytes()):
-#                 final_text = recognizer.Result()
-#                 print(f"Final Transcript: {final_text}")
-#             else:
-#                 interim_text = recognizer.PartialResult()
-#                 print(f"Interim Trancript: {interim_text}")
-#
-# if __name__ == "__main__":
-#     main()
-#
+#Initialize PyAudio
+p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK_SIZE)
 
-print("Recording started...")
-with sd.InputStream(channels=2, samplerate=RATE, blocksize=CHUNK_SIZE) as stream:
-    while True:
-        data, overflow = stream.read(CHUNK_SIZE)
-        if overflow:
-            print("YOUR BREAKING MY CODE WITH OVERFLOW")
+while True:
+    data = stream.read(CHUNK_SIZE)
+    if len(data) == 0:
+        break
 
-        if recognizer.AcceptWaveform(data.tobytes()):
-            final_text = recognizer.Result()
-            print(f"Final Transcript: {final_text}")
-        else:
-            interim_text = recognizer.PartialResult()
-            print(f"Interim Transcript: {interim_text}")
+    #Process audio chunk
+    if recognizer.AcceptWaveform(data):
+        final_text = recognizer.Result()
+        print(f"Final transcript: {final_text}")
+    else:
+        interim_text = recognizer.PartialResult()
+        print(f"Interim transcript: {interim_text}")
+
+#Cleanup
+stream.stop_stream()
+stream.close()
+p.terminate()
